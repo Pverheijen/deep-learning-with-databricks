b2837d52e95e0637f1b31bbebfaf2e3c63daa500
SireInsectus <SireInsectus>
2021-10-19 22:53:03 +0000

    Checkpoint

--- Scalable-Deep-Learning-with-Tensorflow-and-Apache-Spark/Version-3.5.0-IL.scala
+++
@@ -1,8 +0,0 @@
-// Databricks notebook source
-// MAGIC 
-// MAGIC %md # Course: Deep-Learning
-// MAGIC * Version 3.5.0-IL
-// MAGIC * Built 2021-10-19 19:51:19 UTC
-// MAGIC * Git revision: b4a2837ab35e560ef7addfd637a240fbf6466a29
-// MAGIC 
-// MAGIC Copyright © 2021 Databricks, Inc.
