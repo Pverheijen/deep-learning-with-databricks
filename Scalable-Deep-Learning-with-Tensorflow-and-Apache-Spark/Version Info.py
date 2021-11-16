abba2fe2867cba99bba30d839027e71eaae5499b
SireInsectus <SireInsectus>
2021-11-16 17:13:20 +0000

    Published v4.0.2

--- Scalable-Deep-Learning-with-Tensorflow-and-Apache-Spark/Version Info.py
+++ Scalable-Deep-Learning-with-Tensorflow-and-Apache-Spark/Version Info.py
@@ -11,7 +11,7 @@
 # MAGIC 
 # MAGIC * Name: **Scalable Deep Learning with Tensorflow and Apache Spark**
 # MAGIC * Version:  **4.0.2**
-# MAGIC * Built On: **Nov 12, 2021 at 22:50:09 UTC**
+# MAGIC * Built On: **Nov 16, 2021 at 17:11:33 UTC**
 
 # COMMAND ----------
 
